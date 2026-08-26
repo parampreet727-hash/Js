79e57275c5c29891884d39c4441dddee054a14a4
parampreet727 <parampreet727@gmail.com>
2026-08-26 14:21:57 +0530

    String

---
+++ str.js
@@ -0,0 +1,18 @@
+// String Methods
+let fullName = "Parampreet";
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+console.log(fullName.charAt(3));
+console.log(fullName.indexOf("e"));
+console.log(fullName.includes("preet"));
+
+console.log(fullName.slice(3,6));
+console.log(fullName.replace("Parampreet", "Harpreet"));
+
+console.log(fullName.split(" "));
+
+let x = "    Param     "
+console.log(x.trim());
+
