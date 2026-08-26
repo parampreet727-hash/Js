7d44df334f5e9a6c8e447533fb60238d0e5bbb18
parampreet727 <parampreet727@gmail.com>
2026-08-26 22:31:25 +0530

    Arr

--- arr.js
+++ arr.js
@@ -5,7 +5,7 @@ console.log(arr.length);
 
 console.log(arr[3]);
 
-arr.pop()
+arr.pop();
 console.log(arr); // remove last arr
 
 arr.shift();
@@ -17,4 +17,6 @@ console.log(arr); //add new arr in the last
 arr.unshift();
 console.log(arr);
 
-
+let x = ["Param", 25];
+x[1] = 24; //update
+console.log(x);
