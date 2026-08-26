aa4549ba65544d1e1d4ff4c8884fd2b38aac71ea
parampreet727 <parampreet727@gmail.com>
2026-08-26 20:54:13 +0530

    arr

---
+++ arr.js
@@ -0,0 +1,20 @@
+let arr = ["Apple", "Banana", "Mango", "Kiwi"];
+console.log(arr);
+
+console.log(arr.length);
+
+console.log(arr[3]);
+
+arr.pop()
+console.log(arr); // remove last arr
+
+arr.shift();
+console.log(arr); //remove first arr
+
+arr.push("Pineapple");
+console.log(arr); //add new arr in the last
+
+arr.unshift();
+console.log(arr);
+
+
