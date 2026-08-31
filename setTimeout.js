0ea12502285766149552c665078adfc0b08769eb
parampreet727 <parampreet727@gmail.com>
2026-08-31 18:44:52 +0530

    setTimeOut

---
+++ setTimeout.js
@@ -0,0 +1,31 @@
+// //setTimeout
+// console.log("Start");
+// setTimeout(() => {
+//   console.log("Execute code after 2 Sec.");
+// }, 2000);
+// console.log("End");
+
+// //setInterval
+// console.log("Start");
+// setInterval(() => {
+//   console.log("Repeate code every 3 Sec.");
+// }, 3000);
+// console.log("End");
+
+// // clearInterval
+// let count = 0;
+// const interValId = setInterval(() => {
+//   count++;
+//   console.log("Repeate code every 1 Sec.");
+//   if (count === 5) {
+//     clearInterval(interValId);
+//     console.log("Interval Cleared");
+//   }
+// }, 1000);
+
+// //clearTimeout
+// const timeOutId = setTimeout(() => {
+//   console.log("This will not execute");
+// }, 5000);
+// clearTimeout(timeOutId);
+// console.log("Timeout Cleared");
